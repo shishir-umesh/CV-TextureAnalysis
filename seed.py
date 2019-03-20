3cfa6929b82abff88002da5eb61d8e05447428e6
Shishir Umesh <umesh.shishir@gmail.com>
2019-03-20 16:52:56 -0400

    Refactored the seed image function

--- seed.py
+++ seed.py
@@ -4,6 +4,18 @@ from math import ceil, floor
 
 def seed_image(img, seed_size, output_rows, output_cols):
 
+    """
+    INPUT:
+    1. img - Original sample image from the user.
+    2. seed_size - The size of the seed image to be randomly choosen
+    output_rows - The number of rows to be present in the output image
+    output_cols - The number of cols to be present in the input image
+
+    OUTPUT:
+    synthesizedImage - The array with the random seed present at the center
+    filledMap - The array with the regions filled set as 1 and the unfilled regions set as 0.
+    """
+
     #Gets the dimensions of the input image
     (rows,cols) = np.shape(img)
 
@@ -14,35 +26,19 @@ def seed_image(img, seed_size, output_rows, output_cols):
     # A 3-by-3 seed in the center randomly taken from the original image to be synthesized
     seed = img[randomRow:randomRow + seed_size, randomCol:randomCol + seed_size]
 
+    #Creates an empty array with zeros to store the new synthesized image
     synthesizedImage = np.zeros((output_rows,output_cols))
 
     #Get the coordinates of the center of the original image
     center_row = floor(output_rows / 2)
     center_col = floor(output_cols / 2)
     half_seedSize = floor(seed_size/2)
+    #Stores the random seed at the center of the new image array
     synthesizedImage[center_row - half_seedSize : center_row + half_seedSize+1, center_col - half_seedSize : center_col + half_seedSize+1] = seed
 
-    #Create a map of filled regions and set it to zeros
+    #Create a map of filled regions with 0 and sets the regions with the seed as 1.
     filledMap = np.zeros((output_rows,output_cols),dtype=np.int8)
     filledMap[center_row - half_seedSize : center_row + half_seedSize+1, center_col - half_seedSize : center_col + half_seedSize+1] = np.ones((seed_size, seed_size))
 
+    #Returns the new image and the filled map arrays
     return synthesizedImage,filledMap
-
-
-def findMatches(template,convPatches, valid_mask, gauss_mask, windowSize, halfWindow, ErrThreshold):
-
-    #Reshaping them into a coloumn vector
-    template = np.reshape(template, windowSize*windowSize)
-    gauss_mask = np.reshape(gauss_mask, windowSize*windowSize)
-    valid_mask = np.reshape(valid_mask, windowSize*windowSize)
-    # PSEUDO CODE ----->   TotWeight = sum i,j GaussiMask(i,j)*ValidMask(i,j)
-    total_weight = np.sum(np.multiply(gauss_mask, valid_mask))
-    # PSEUDO CODE ----->   dist = (Template(ii,jj)-SampleImage(i-ii,j-jj))^2
-    distance = (convPatches-template)**2
-    # PSEUDO CODE ----->   SSD(i,j) = SSD(i,j) + dist*ValidMask(ii,jj)*GaussMask(ii,jj)
-    # PSEUDO CODE ----->   SSD(i,j) = SSD(i,j) / TotWeight
-    ssd = np.sum((distance*gauss_mask*valid_mask) / total_weight, axis=1)
-    min_error = min(ssd)
-    # print "Min err mat= "+str(min_error)
-    mid = int(((2 * halfWindow + 1) ** 2) / 2)
-    return [[err, convPatches[i][mid]] for i, err in enumerate(ssd) if err <= min_error*(1+ErrThreshold)]
